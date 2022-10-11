7fba1ca5974fd7e144819d86ca4066c9cd3db5eb
annied222 <annie.donnelly90@gmail.com>
2022-10-11 13:35:40 -0500

    adding budget analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,24 +8,46 @@
 import os
 import csv 
 
-csvpath=os.path.join('Resources','budget_data.csv')
-with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile,delimiter=",")
-    print(csvreader)
+csvpath=os.path.join("Resources","budget_data.csv")
+#budget_csv=os.path.join("..","Resources","budget_data.csv"
 
+totalmonths=[]
+nettotal=[]
+profitlosses=[]
+rowcount=0
+
+with open(csvpath) as csvfile:
+    
+    csvreader=csv.reader(csvfile)
+    
     #Header in csv
     csvheader=next(csvreader)
+    
+    for row in csvreader:
+        totalmonths.append(row[0])
+        nettotal.append(int(row[1]))
+    for i in range(len(totalmonths)-1):
+        profitlosses.append(int(nettotal[i+1]-nettotal[i]))
 
-    totalmonths=0
+print('\n')
+print("Financial Analysis")
+print("-"*50)
+print(f"Total Months: {len(totalmonths)}")
+print(f"Total: ${sum(nettotal)}")
+print(f"Average Change: ${int(sum(profitlosses)/(rowcount-1))}")
+print(f"Greatest Increase in Profits: {totalmonths[profitlosses.index(max(profitlosses))+1]} (${max(profitlosses)})")
+print(f"Greatest Increase in Profits: {totalmonths[profitlosses.index(min(profitlosses))+1]} (${min(profitlosses)})")
+print('\n')
 
-    print('\n')
-    print("Financial Analysis")
+outputpath=os.path.join("analysis","budget_analysis.txt")
 
-    print("-" * 25)
-
-    #Total number of months in dataset
-    For row in csvreader:
-        print(row)
-        
-
-    print(f"Total Months:"{totalmonths})
+with open(outputpath,'w') as csvfile:
+    csvwriter=csv.writer(csvfile)
+    csvwriter.writerow(['Financial Analysis'])
+    csvwriter.writerow(["-"*50])
+    csvwriter.writerow([f"Total Months: {len(totalmonths)}"])
+    csvwriter.writerow([f"Total: ${sum(nettotal)}"])
+    csvwriter.writerow([f"Average Change: ${int(sum(profitlosses)/(rowcount-1))}"])
+    csvwriter.writerow([f"Greatest Increase in Profits: {totalmonths[profitlosses.index(max(profitlosses))+1]} (${max(profitlosses)})"])
+    csvwriter.writerow([f"Greatest Increase in Profits: {totalmonths[profitlosses.index(min(profitlosses))+1]} (${min(profitlosses)})"])
+    
